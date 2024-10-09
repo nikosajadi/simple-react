9dcc5e439a89215ab7ebe53ef0ce07e129dbc07f
nikosajadi <nasibeh.sajjadi@gmail.com>
2024-10-09 14:47:24 +0200

    compnents

---
+++ src/components/App/App.js
@@ -0,0 +1,17 @@
+import Button from '../Button/Button';
+import Header from '../Header/Header';
+import './App.css';
+
+const App = () => {
+  return (
+    <div className="App">
+            <Header />
+      <h1>This is a simple project but it is going to be perfect</h1>
+      <p>It would be great</p>
+      <Button />
+
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Header/Header.js
@@ -0,0 +1,22 @@
+
+import './Header.css'
+
+
+const Header = () => {
+   
+  return (
+    <div className="Header">
+<ul>
+  <li>
+    <p>
+      About 
+      contact
+      
+    </p>
+  </li>
+</ul>
+    </div>
+  );
+};
+
+export default Header
