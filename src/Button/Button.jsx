806c05d7bbb3a5eddf3753cd0861210ca49d3157
nikosajadi <nasibeh.sajjadi@gmail.com>
2024-10-10 16:00:30 +0200

    Add event in ContactForm

--- src/Button/Button.jsx
+++ src/Button/Button.jsx
@@ -1,9 +1,11 @@
-import React from 'react';
-import './Button.css';
-
-const Button = ({children}) => {
+// The handleClick prop is given a default value of an empty function to avoid errors.
+// If no handleClick function is provided when using this Button component, 
+// the empty function will be used, preventing errors when the button is clicked.
+const Button = ({ children, handleClick = () => {} }) => {
   return (
-    <button className='button'>{children}</button>
+    <button onClick={handleClick} className='button'>
+      {children}
+    </button>
   );
 }
 
--- src/Contact/Contact.js
+++ src/Contact/Contact.js
@@ -1,9 +1,31 @@
-import React from "react";
+import React, { Children } from "react";
+import Button from "../Button/Button";
 
 const Contact = () => {
+  const HandleChangeInputText = (event) =>{
+    console.log('event', event.target.value)
+  }
+  const HandleChangeTextArea =(event)=>{
+    console.log('textarea', event.target.value)
+  }
+  const HandleSubmit = () =>{
+    console.log('Submitted')
+  }  
+
   return (
-    <div>
+    <div className="Contact">
       <p>Contact me at: <a href="mailto:Nasibeh.sajjadi@gmail.com">Nasibeh.sajjadi@gmail.com</a></p>
+
+      <div className="formControl">
+      <input onChange={HandleChangeInputText} type="text" placeholder="subject" />
+      </div>
+      <div className="formControl">
+          <textarea onChange={HandleChangeTextArea}>Your Request</textarea>
+      </div>
+      <div className="formControl">
+      <Button handleClick={HandleSubmit} >Submit</Button>
+      </div> 
+     
     </div>
   );
 }
