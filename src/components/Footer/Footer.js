5e33a5fd715f9e50f4122ca3f5df18ee1f49f337
nikosajadi <nasibeh.sajjadi@gmail.com>
2024-10-22 17:02:53 +0200

    [Update]UI&UX

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -9,8 +9,11 @@ const Footer = () => {
     <div className="Footer">
       <h5>Developed By Niko</h5>
       <div>
+      <h6>change Theme:</h6>
+      <div>
         <button onClick={() => themValues.setActiveTheme('green')} className="green">Green</button>
         <button onClick={() => themValues.setActiveTheme('blue')} className="blue">Blue</button>
+      </div>
       </div>
     </div>
   );
