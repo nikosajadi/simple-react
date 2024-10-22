beaa433352f4f5959e7e48a09320011882c1c2e9
nikosajadi <nasibeh.sajjadi@gmail.com>
2024-10-22 15:43:51 +0200

    [Update]UI&UX design

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -2,22 +2,18 @@ import React, { useContext } from "react";
 import './Footer.css';
 import ThemeContext from "../../contexts/ThemeContext";
 
-
 const Footer = () => {
+  const themValues = useContext(ThemeContext);
   
-  const themValues = useContext(ThemeContext)
-   console.log(":themeValuss", themValues)
-
-  return(
+  return (
     <div className="Footer">
       <h5>Developed By Niko</h5>
       <div>
-         <button onClick={()=> themValues.setActiveTheme('green')} className="green">green</button>
-         <button onClick={()=> themValues.setActiveTheme('blue')} className="blue">blue</button>
+        <button onClick={() => themValues.setActiveTheme('green')} className="green">Green</button>
+        <button onClick={() => themValues.setActiveTheme('blue')} className="blue">Blue</button>
       </div>
     </div>
-  )
-
+  );
 };
 
 export default Footer;
